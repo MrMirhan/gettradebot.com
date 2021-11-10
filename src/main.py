fcbfc32e2e019df5afd91821dd31ff44f6405700
Mirhan <hamzamirhan.almendi@std.yeditepe.edu.tr>
2021-11-10 16:47:10 +0300

    Developing

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import os, sys
+import config
+import logging
+import threading
+from updater import UpdateBot
+updater = UpdateBot()
+def webThread():
+    from web.main import create_app
+    app = create_app()
+    app.run(host=config.IP, port=config.PORT, debug=config.DEBUG)
+
+def loggs():
+    from logging_start import logging_start as lstart
+    lstart()
+
+def start():
+    loggs()
+    logger=logging.getLogger()
+    logger.info("Current branch: {} & Github branch: {}".format(updater.currentBranch, updater.repoBranch))
+    logger.info("Current version: {} & Github version: {}".format(updater.currentVersion, updater.repoVersion))
+    threading.Thread(target=webThread, daemon=True, args=( )).start()
+    logger.info("Program started successfully.")
+    
--- updater.py
+++ updater.py
@@ -1,15 +1,43 @@
-import config
+from src import config
 from github import Github
 import logging
 logger = logging.getLogger()
+import multiprocessing, os
 
-
-def check():
-    currentVersion = config.VER
-    currentBranch = config.BRANCH
-    g = Github(config.ACCESS_KEY)
-    repo = g.get_repo("MrMirhan/gettradebot.com")
-    repoBranch = repo.default_branch
-    repoConfig = repo.get_contents("config.py").content
-    if not repoBranch == currentBranch:
-        logger.warning("New branch availible! Update required.")
+class UpdateBot():
+    def __init__(self):
+        self.warnedBranch = 0
+        self.warnedVersion = 0
+        self.check()
+    
+    def check(self):
+        self.currentVersion = config.VER
+        self.currentBranch = config.BRANCH
+        self.g = Github(config.ACCESS_KEY)
+        self.repo = self.g.get_repo("MrMirhan/gettradebot.com")
+        self.repoBranch = self.repo.default_branch
+        self.repoConfig = (self.repo.get_contents("config.py").decoded_content).decode("utf-8").split("\n")
+        self.repoVersion = (str([ x for x in self.repoConfig if "VER" in x][0]).replace("VER = ", "")).replace('"', '')
+        if not self.repoBranch == self.currentBranch:
+            if self.warnedBranch == 0:
+                logger.warning("New branch availible! Update required.")
+                self.warnedBranch = 1
+        else:
+            if self.warnedBranch == 1:
+                logger.info("Update completed.")
+                logger.info("Current branch: {} & Github branch: {}".format(self.currentBranch, self.repoBranch))
+                logger.info("Current version: {} & Github version: {}".format(self.currentVersion, self.repoVersion))
+                self.warnedBranch = 0
+        if self.repoVersion > self.currentVersion:
+            if self.warnedVersion == 0:
+                logger.warning("New version published! Update required.")
+                self.warnedVersion = 1
+        else:
+            if self.warnedVersion == 1:
+                logger.info("Update completed.")
+                logger.info("Current branch: {} & Github branch: {}".format(self.currentBranch, self.repoBranch))
+                logger.info("Current version: {} & Github version: {}".format(self.currentVersion, self.repoVersion))
+                self.warnedVersion = 0
+        
+    def purge(self):
+        os.remove("./src")
